73868268e0f26581f6f0636e42879d7da744262d
akbiv <akbivash@gmail.com>
2023-01-21 14:44:05 +0545

    -a

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).currentUser).token
+const TOKEN = process.env.REACT_APP_TOKEN
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).currentUser).token
 export const publicRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
     
